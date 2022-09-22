52c146ea6e5dcf01e582d6e32aec842b19211940
E-Trinh <ethantrinh@csus.edu>
2022-09-22 14:17:37 -0700

    Added javascript text styling functionality

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,1 +1,34 @@
-console.log("test")
+const textArea = document.getElementById("text");
+
+if (document.getElementById("fancy").checked) {
+    makeFancy(null);
+}
+if (document.getElementById("boring").checked) {
+    makeBoring(null);
+}
+
+document.getElementById("bigger").onclick = e => {
+    textArea.style.fontSize = "4em";
+};
+
+document.getElementById("moo").onclick = moo;
+document.getElementById("fancy").onchange = makeFancy
+document.getElementById("boring").onchange = makeBoring;
+
+function moo(e) {
+    var str = textArea.value.toUpperCase();
+    var parts = str.split(".");
+    textArea.value = parts.join("-Moo");
+}
+
+function makeFancy(e) {
+    textArea.style.fontWeight = "bold";
+    textArea.style.color = "blue";
+    textArea.style.textDecoration = "underline";
+}
+
+function makeBoring(e) {
+    textArea.style.fontWeight = "400";
+    textArea.style.color = "black";
+    textArea.style.textDecoration = "none";
+}
